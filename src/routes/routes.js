f71d1bde4341f3fc54f2e64c555018e5c66bec22
vivianiHarima <viviharima@hotmail.com>
2024-08-02 22:05:39 -0300

    atualização das rotas e correções no LocalController

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,11 +3,12 @@ const { Router } = require('express');
 const usuariosRoutes = require('./usuarios.routes');
 const locaisRoutes = require('./locais.routes');
 const loginRoutes = require('./login.routes');
+const verificaToken = require('../middlewares/verificaToken');
 
 const routes = new Router();
 
 routes.use('/usuarios', usuariosRoutes);
-routes.use('/locais', locaisRoutes);
+routes.use('/locais',verificaToken, locaisRoutes);
 routes.use('/login', loginRoutes)
 
 
