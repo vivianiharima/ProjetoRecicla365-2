f930ef3c4895ab47b445e9477ed6e9f981272889
vivianiHarima <viviharima@hotmail.com>
2024-08-03 01:13:23 -0300

    corrigindo e atualizando documentação swagger

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -4,7 +4,21 @@ const LoginController = require('../controllers/LoginController')
 const loginRoutes = new Router();
 
 
-loginRoutes.post('/', LoginController.logar);
+loginRoutes.post('/', LoginController.logar
+     /*
+        #swagger.tags = ['Usuários'],
+        #swagger.description = 'Para logar um usuário',
+        #swagger.parameters['loginUsuario'] = {
+            in: 'body',
+            description: 'Login de usuário',
+            required: true,
+            schema: { 
+                $email: "teste@gmail.com",
+                $senha: "teste123"
+            }
+        }
+    */
+);
 
 
 module.exports = loginRoutes
