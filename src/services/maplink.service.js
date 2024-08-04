62d595302033b31df2a5a8497b3ee43e5a4c01da
vivianiHarima <viviharima@hotmail.com>
2024-08-04 16:43:08 -0300

    alterações para função de gerar link funcionar corretamente

--- src/services/maplink.service.js
+++ src/services/maplink.service.js
@@ -5,14 +5,16 @@ const axios = require('axios');
         const urlMapaApi = 'https://nominatim.openstreetmap.org/search?format=json&country=Brazil&limit=1'
 
         try {
-            const response = await axios.getAdapter(`${urlMapaApi}&postalcode=${cep}`);
+            const response = await axios.get(`${urlMapaApi}&postalcode=${cep}`);
             const data = response.data;
              
             if(!data || !data.lenght === 0){
                 throw new Error(`Não foi possível encontrar coordenadas para o CEP: ${cep}. Verifique se o CEP está correto ou tente um CEP diferente.`);
             }
-
+            
             const { lat, lon, display_name } = data[0]
+
+            console.log('Dados da API:', data);
 
             if(!lat || !lon || !display_name){
                 throw new Error(`Não foi possível encontrar os dados de localização. Por favor, verifique se os detalhes fornecidos estão corretos`);
@@ -32,11 +34,10 @@ const axios = require('axios');
 
     async function gerarLinkGoogleMaps(latitude, longitude){
         try {
-            const { latitude, longitude } = await pegarCoordenadasPeloCep(cep)
-            return `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
             
+            return `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+        
         } catch (error) {
-
                 console.error('Erro ao obter link do Google Maps:', error.message);
                 throw new Error(`Erro gerar o link do Google Maps: ${error.message}`);
         }
